6f3cfffeca175c9c01cdd96484fd1f207aab768a
Maulik Sharma <sharmamaulik95@gmail.com>
2025-07-28 01:32:01 +0530

    5.0 Added about and dark mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="ReactApp" about="Nice" />
       <div className="container my-3">
-        <TextForm heading="Enter text to analyze below" />
+        {/* <TextForm heading="Enter text to analyze below" /> */}
+        <About />
       </div>
     </>
   );
